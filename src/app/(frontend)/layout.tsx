e897cff408c82a0e96c95e011fa60f84111906a1
Baslarsa <davvelars@gmail.com>
2026-02-16 21:54:24 +0200

    add ga

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -11,12 +11,25 @@ import { mergeOpenGraph } from '@/utilities/mergeOpenGraph'
 import './globals.css'
 import { getServerSideURL } from '@/utilities/getURL'
 import CustomLoadingOverlay from '@/components/LoadingOverlay'
-
+import Script from 'next/script'
+const GA_ID = process.env.NEXT_PUBLIC_GA_ID!
 export default async function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <ViewTransitions>
       <html lang="en" suppressHydrationWarning>
         <head>
+          <Script
+            src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+            strategy="afterInteractive"
+          />
+          <Script id="ga-gtag" strategy="afterInteractive">
+            {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){window.dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_ID}', { anonymize_ip: true });
+          `}
+          </Script>
           <InitTheme />
           <link href="/js-favicon.png" rel="icon" sizes="32x32" />
           <link href="/js-favicon.svg" rel="icon" type="image/svg+xml" />
