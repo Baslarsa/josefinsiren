dcab22f1bed6b5838de7f2018a16289a959b8b44
Baslarsa <davvelars@gmail.com>
2026-07-03 21:19:41 +0300

    fix hero bg color

--- src/heros/HighImpact/index.tsx
+++ src/heros/HighImpact/index.tsx
@@ -9,13 +9,9 @@ import { SetHeaderTheme } from './SetHeaderTheme'
 
 export const HighImpactHero: React.FC<Page['hero']> = ({ links, media, richText }) => {
   return (
-    <div
-      className="relative -mt-[10rem] flex items-center justify-center text-neutral-200"
-      data-theme="dark"
-    >
-      <SetHeaderTheme theme="dark" />
-      <div className="container mb-8 z-[5] relative flex items-center justify-center">
-        <div className="max-w-[36.5rem] text-center">
+    <div className="flex items-center justify-center text-neutral-200 pt-8">
+      <div className="my-auto container z-[5] relative flex items-center justify-center">
+        <div className="text-center">
           {richText && <RichText className="mb-6" data={richText} enableGutter={false} />}
           {Array.isArray(links) && links.length > 0 && (
             <ul className="flex md:justify-center gap-4">
@@ -30,12 +26,6 @@ export const HighImpactHero: React.FC<Page['hero']> = ({ links, media, richText 
           )}
         </div>
       </div>
-      <div className="min-h-[100vh] select-none">
-        {media && typeof media === 'object' && (
-          <Media fill imgClassName="object-cover" priority resource={media} />
-        )}
-      </div>
-      <div className="absolute inset-0 bg-black opacity-30" />
     </div>
   )
 }
